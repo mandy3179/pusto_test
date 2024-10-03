35b4b74ddcd712b58c84033b9a3d277ee45ee19b
mandy3179 <mandy3179@tutamail.com>
2024-10-04 03:43:14 +1000

    Add logic for awarding a prize after completing a level

--- second_task/models.py
+++ second_task/models.py
@@ -1,6 +1,15 @@
+import random
+from typing import Optional
+
+
 from django.db import models
+from django.db.transaction import atomic
+from django.utils import timezone
+
 
 class Player(models.Model):
+    # тут можно использовать one_to_one с user или переопределить его
+    # так же можно использовать id, который  по умолчанию генерируется
     player_id = models.CharField(max_length=100)
 
 
@@ -9,9 +18,8 @@ class Level(models.Model):
     order = models.IntegerField(default=0)
 
 
-
 class Prize(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=52)  # добавил max_length
 
 
 class PlayerLevel(models.Model):
@@ -20,6 +28,24 @@ class PlayerLevel(models.Model):
     completed = models.DateField()
     is_completed = models.BooleanField(default=False)
     score = models.PositiveIntegerField(default=0)
+
+    @atomic
+    def award_prize(self) -> Optional[str]:
+        """
+        Присваивает случайный приз игроку за успешное завершение уровня.
+        
+        Returns:
+            str: Название присвоенного приза, если уровень завершен и приз был выдан.
+            None: Если уровень не завершен или призов нет.
+        """
+        if self.is_completed:
+            level_prizes = LevelPrize.objects.filter(level=self.level)
+            if level_prizes.exists():
+                random_prize = random.choice(level_prizes)
+                random_prize.received = timezone.now().date()
+                random_prize.save()
+                return random_prize.prize
+        return None
 
 
 class LevelPrize(models.Model):
